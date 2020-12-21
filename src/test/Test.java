d485448c4df30ba8c4e69b2a6eb30484403de924
David <david.barriofu@elorrieta-errekamari.com>
2020-12-21 23:55:20 +0100

    Clase de inserción en base de datos y test

---
+++ src/test/Test.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import hiberclass.Estaciones;
+import main.Insertado;
+
+public class Test {
+	
+	Insertado insertado = new Insertado();
+
+	@org.junit.Test
+	public void testInsercion() {
+		Estaciones estacion = new Estaciones();
+		estacion.setNombre("Prueba");
+		estacion.setDireccion("Dire de prueba");
+		assertEquals(true, Insertado.insertarEstacion(estacion, true));
+	}
+
+}
