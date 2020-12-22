ce3fed5c345bd9f9a4f318200b02a217206d6c5e
David <david.barriofu@elorrieta-errekamari.com>
2020-12-22 02:16:21 +0100

    Añadidos más tests. 84,5% de coverage.

--- src/test/Test.java
+++ src/test/Test.java
@@ -27,30 +27,16 @@ public class Test {
 	
 	@org.junit.Test
 	public void testInsercionEstacion() {
-		estacion = new Estaciones("prueba", "prueba", "prueba", Double.valueOf("1"), Double.valueOf("1"), Double.valueOf("1"), Double.valueOf("1"), set);
 		estacion = new Estaciones("prueba", "prueba");
-		estacion.setNombre("Prueba");
-		estacion.setDireccion("Dire de prueba");
-		estacion.setProvincia("prueba");
-		estacion.setCoordenadaX(Double.valueOf("1"));
-		estacion.setCoordenadaY(Double.valueOf("1"));
-		estacion.setLatitud(Double.valueOf("1"));
-		estacion.setLongitud(Double.valueOf("1"));
+		estacion = new Estaciones("prueba", "prueba", "prueba", Double.valueOf("1"), Double.valueOf("1"), Double.valueOf("1"), Double.valueOf("1"), set);
 		assertEquals(true, InsertarBorrar.insertar((Object)estacion));
 		InsertarBorrar.borrar(estacion);
 	}
 	
 	@org.junit.Test
 	public void testInsercionMunicipio() {
-		Set set = new HashSet(0);
-		municipio = new Municipios("prueba", "prueba", Double.valueOf("1"), Double.valueOf("1"), 1, set);
-		municipio = new Municipios();
 		municipio = new Municipios("prueba");
-		municipio.setNombre("prueba");
-		municipio.setDescripcion("prueba");
-		municipio.setLatitud(Double.valueOf("1"));
-		municipio.setLongitud(Double.valueOf("1"));
-		municipio.setCodigo(1);
+		municipio = new Municipios("prueba", "prueba", Double.valueOf("1"), Double.valueOf("1"), 1, set);
 		assertEquals(true, InsertarBorrar.insertar((Object)municipio));
 		InsertarBorrar.borrar(municipio);
 	}
@@ -58,19 +44,23 @@ public class Test {
 	@org.junit.Test
 	public void testInsercionInformes() {
 		
-		estacion = new Estaciones();
 		estacion.setNombre("prueba");
 		estacion.setDireccion("prueba");
 		informe = new Informes(estacion, "prueba");
 		informe = new Informes(estacion, "prueba", "prueba", set);
-		informe.setEstaciones(estacion);
-		informe.setFormato("prueba");
-		informe.setUrl("prueba");
-		informe.setHorarios(set);
 		InsertarBorrar.insertar((Object)estacion);
 		assertEquals(true, InsertarBorrar.insertar((Object)informe));
 		InsertarBorrar.borrar(informe);
 		InsertarBorrar.borrar(estacion);
+	}
+	
+	@org.junit.Test
+	public void testInsercionEntorno() {
+		
+		entorno = new Entornos("prueba");
+		entorno = new Entornos("prueba", "prueba", "prueba", "prueba", Double.valueOf("1"), Double.valueOf("1"), set);
+		assertEquals(true, InsertarBorrar.insertar((Object)entorno));
+		InsertarBorrar.borrar(entorno);
 	}
 
 }
